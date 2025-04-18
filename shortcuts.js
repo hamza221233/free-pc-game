708005d5dc3086b34a7c3a0c6f22cbaf36230258
hamza221233 <hamzadls23.maroc@gmail.com>
2025-04-18 20:12:00 +0100

    Add files via upload

---
+++ shortcuts.js
@@ -0,0 +1,13 @@
+window.shortcuts = {
+    'gow': 'God of War',
+    'gowr': 'God of War Ragnarok',
+    'cod': 'Call of Duty',
+    'fifa': 'FIFA',
+    'بيس 21': 'eFootball PES 2021',
+    'pes': 'eFootball PES 2021',
+    'ac': 'Assassin\'s Creed',
+    'gta': 'Grand Theft Auto',
+    'nf': 'Need for Speed',
+    'bf': 'Battlefield',
+    'tlou': 'The Last of Us'
+};
